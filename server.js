9be859bc1721d7e291a8c227657fdec15788ab21
Jerome <iecepursmsc7@gmail.com>
2026-02-10 02:25:13 +0800

    Use Render PORT

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const SQLiteStore = require("connect-sqlite3")(session);
 const db = require("./db");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
